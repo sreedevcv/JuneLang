77b4f52f18093120fce46614aa26cc2e188d7243
sreedev <sreedevcv0@gmail.com>
2024-06-10 06:58:00 +0000

    Changed references to pointers

--- main.cpp
+++ main.cpp
@@ -9,15 +9,18 @@ int main()
     jl::Token::TokenType t2 = jl::Token::TokenType::FLOAT;
     jl::Token::TokenType t3 = jl::Token::TokenType::PLUS;
     std::string lx = "+";
-    jl::Token::Value v1(123);
-    jl::Token::Value v2(43.43);
-    jl::Expr* l1 = new jl::Literal(v1, t1);
-    jl::Expr* l2 = new jl::Literal(v2, t2);
-    jl::Token tok = jl::Token(t3, lx, 1);
-    jl::Binary b1(*l1, tok, *l2);
+    // jl::Token::Value v1(123);
+    // jl::Token::Value v2(43.43);
+    // jl::Expr* l1 = new jl::Literal(v1, t1);
+    // jl::Expr* l2 = new jl::Literal(v2, t2);
+    // jl::Token tok = jl::Token(t3, lx, 1);
+    // jl::Binary b1(*l1, tok, *l2);
 
-    jl::Unary u(tok, b1);
+    // jl::Unary u(tok, b1);
+    jl::Unary u(new jl::Token(t3, lx, 1), new jl::Binary(new jl::Literal(new jl::Token::Value(123), t1), new jl::Token(t3, lx, 1), new jl::Literal(new jl::Token::Value(12.5), t2)));
 
     jl::IExprVisitor* visitor = new jl::ParsetreePrinter();
-    u.accept(*visitor);
+    u.accept(*visitor, visitor->get_context());
+    std::string* context = (std::string*)visitor->get_context();
+    std::cout << *context << "\n";
 }
--- src/Expr.hpp
+++ src/Expr.hpp
@@ -15,119 +15,127 @@ class Literal;
 
 class IExprVisitor {
 public:
-    virtual void visitAssignExpr(Assign& expr) = 0;
-    virtual void visitBinaryExpr(Binary& expr) = 0;
-    virtual void visitGroupingExpr(Grouping& expr) = 0;
-    virtual void visitUnaryExpr(Unary& expr) = 0;
-    virtual void visitLiteralExpr(Literal& expr) = 0;
+    virtual void visitAssignExpr(Assign* expr, void* context) = 0;
+    virtual void visitBinaryExpr(Binary* expr, void* context) = 0;
+    virtual void visitGroupingExpr(Grouping* expr, void* context) = 0;
+    virtual void visitUnaryExpr(Unary* expr, void* context) = 0;
+    virtual void visitLiteralExpr(Literal* expr, void* context) = 0;
+
+    virtual void* get_context() = 0;
 };
 
 class Expr {
 public:
-    virtual void accept(IExprVisitor& visitor) = 0;
+    virtual void accept(IExprVisitor& visitor, void* context) = 0;
 };
 
 class Assign : public Expr {
 public:
-    inline virtual void accept(IExprVisitor& visitor) override
+    inline virtual void accept(IExprVisitor& visitor, void* context) override
     {
-        visitor.visitAssignExpr(*this);
+        visitor.visitAssignExpr(this, context);
     }
 };
 
 class Binary : public Expr {
 public:
-    Expr& m_left;
-    Token& m_oper;
-    Expr& m_right;
+    Expr* m_left;
+    Token* m_oper;
+    Expr* m_right;
 
-    inline Binary(Expr& left, Token& oper, Expr& right)
+    inline Binary(Expr* left, Token* oper, Expr* right)
         : m_left(left)
         , m_right(right)
         , m_oper(oper)
     {
     }
 
-    inline virtual void accept(IExprVisitor& visitor) override
+    inline virtual void accept(IExprVisitor& visitor, void* context) override
     {
-        visitor.visitBinaryExpr(*this);
+        visitor.visitBinaryExpr(this, context);
     }
 };
 
 class Grouping : public Expr {
 public:
-    Expr& m_expr;
+    Expr* m_expr;
 
-    inline Grouping(Expr& expr)
+    inline Grouping(Expr* expr)
         : m_expr(expr)
     {
     }
-    inline virtual void accept(IExprVisitor& visitor) override
+    inline virtual void accept(IExprVisitor& visitor, void* context) override
     {
-        visitor.visitGroupingExpr(*this);
+        visitor.visitGroupingExpr(this, context);
     }
 };
 
 class Literal : public Expr {
 public:
-    Token::Value m_value;
+    Token::Value* m_value;
     Token::TokenType m_type;
 
-    inline Literal(Token::Value value, Token::TokenType type)
+    inline Literal(Token::Value* value, Token::TokenType type)
         : m_value(value)
         , m_type(type)
     {
     }
 
-    inline virtual void accept(IExprVisitor& visitor) override
+    inline virtual void accept(IExprVisitor& visitor, void* context) override
     {
-        visitor.visitLiteralExpr(*this);
+        visitor.visitLiteralExpr(this, context);
     }
 };
 
 class Unary : public Expr {
 public:
-    Expr& m_expr;
-    Token& m_oper;
-    inline Unary(Token& oper, Expr& expr)
+    Expr* m_expr;
+    Token* m_oper;
+
+    inline Unary(Token* oper, Expr* expr)
         : m_expr(expr)
         , m_oper(oper)
     {
     }
 
-    inline virtual void accept(IExprVisitor& visitor) override
+    inline virtual void accept(IExprVisitor& visitor, void* context) override
     {
-        visitor.visitUnaryExpr(*this);
+        visitor.visitUnaryExpr(this, context);
     }
 };
 
+
 class ParsetreePrinter : public IExprVisitor {
 public:
-    inline void visitAssignExpr(Assign& expr) override
+    std::string context;
+
+    inline void visitAssignExpr(Assign* expr, void* context) override
     {
     }
-    inline void visitBinaryExpr(Binary& expr) override
+    inline void visitBinaryExpr(Binary* expr, void* context) override
     {
-        parenthesize(expr.m_oper.get_lexeme(), { &expr.m_left, &expr.m_right });
+        parenthesize(expr->m_oper->get_lexeme(), { expr->m_left, expr->m_right });
     }
-    inline void visitGroupingExpr(Grouping& expr) override
+    inline void visitGroupingExpr(Grouping* expr, void* context) override
     {
     }
-    inline void visitUnaryExpr(Unary& expr) override
+    inline void visitUnaryExpr(Unary* expr, void* context) override
     {
-        parenthesize(expr.m_oper.get_lexeme(), { &expr.m_expr });
+        parenthesize(expr->m_oper->get_lexeme(), { expr->m_expr });
     }
-    inline void visitLiteralExpr(Literal& expr) override
+    inline void visitLiteralExpr(Literal* expr, void* context) override
     {
-        switch (expr.m_type) {
+        std::string* cntx = reinterpret_cast<std::string*>(context);
+
+        switch (expr->m_type) {
         case Token::INT:
-            std::cout << std::get<int>(expr.m_value);
+            cntx->append(std::to_string(std::get<int>(*(expr->m_value))));
             break;
         case Token::FLOAT:
-            std::cout << std::get<double>(expr.m_value);
+            cntx->append(std::to_string(std::get<double>(*(expr->m_value))));
             break;
         case Token::STRING:
-            std::cout << std::get<std::string>(expr.m_value);
+            cntx->append(std::get<std::string>(*(expr->m_value)));
             break;
 
         default:
@@ -137,12 +145,18 @@ public:
 
     inline void parenthesize(std::string name, std::vector<Expr*> exprs)
     {
-        std::cout << "(" << name << " ";
+        context.append("(");
+        context.append(name);
         for (const auto exp : exprs) {
-            std::cout << " ";
-            exp->accept(*this);
+            context.append(" ");
+            exp->accept(*this, &context);
         }
-        std::cout << ")";
+        context.append(")");
+    }
+
+    inline virtual void* get_context()
+    {
+        return &context;
     }
 };
 
